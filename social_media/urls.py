5eb1c755a49b42cf1854d553be86f0ebff68c8b6
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2022-07-20 12:18:24 -0300

    First build with static files added

---
+++ social_media/urls.py
@@ -0,0 +1,11 @@
+import imp
+from django.contrib import admin
+from django.urls import path
+
+from .views import index_view
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index_view)
+]
